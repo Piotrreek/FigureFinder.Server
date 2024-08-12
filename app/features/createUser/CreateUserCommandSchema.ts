e450f15e7e36f97509f547b360510dc45e1fe450
Piotr Gębalski <piotrgebalski5@gmail.com>
2024-08-12 13:41:22 +0200

    Add create user command schema

---
+++ app/features/createUser/CreateUserCommandSchema.ts
@@ -0,0 +1,24 @@
+import { object, string } from "yup";
+import { FormSchema } from "../../../utils/FormSchema";
+import { CreateUserCommand } from "./CreateUserCommand";
+
+const alphanumericRegex = /^[a-zA-Z0-9]*$/;
+const noSpacesRegex = /^\S*$/;
+
+export const CreateUserCommandSchema = object().shape<
+  FormSchema<CreateUserCommand>
+>({
+  email: string()
+    .email("You must provide correct email address")
+    .required("Email is required"),
+  username: string()
+    .matches(
+      alphanumericRegex,
+      "Username must contain only alphanumeric characters"
+    )
+    .required("Username is required"),
+  password: string()
+    .matches(noSpacesRegex, "Password cannot contain spaces")
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+});
