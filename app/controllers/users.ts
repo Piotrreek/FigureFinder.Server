977e789b96d2ec1f82cd55ce723370eb3d7343e6
Piotr Gębalski <piotrgebalski5@gmail.com>
2024-08-13 09:28:40 +0200

    Add controller code for creating user

--- app/controllers/users.ts
+++ app/controllers/users.ts
@@ -1,9 +1,18 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import CreateUserCommandHandler from "../features/createUser/CreateUserCommandHandler";
+import { CreateUserCommandMapper } from "../features/createUser/CreateUserCommandMapper";
 
-const createUser = async (req: Request, res: Response) => {
-    
-}
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const handler = new CreateUserCommandHandler();
+  const mapper = new CreateUserCommandMapper();
+  try {
+    const id = await handler.handle(mapper.map(req.body));
+    res.status(201).json({ id: id });
+  } catch (err: unknown) {
+    next(err);
+  }
+};
 
 export default {
-    createUser
+  createUser,
 };
