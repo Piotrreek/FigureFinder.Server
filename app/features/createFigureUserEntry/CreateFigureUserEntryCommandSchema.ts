9642af7620eb0c46858f5589a1af83d65d29b4df
Piotr Gębalski <piotrgebalski5@gmail.com>
2024-08-15 08:44:19 +0200

    Create validation schema for command

---
+++ app/features/createFigureUserEntry/CreateFigureUserEntryCommandSchema.ts
@@ -0,0 +1,12 @@
+import { date, number, object, string } from "yup";
+import { FormSchema } from "../../../utils/FormSchema";
+import { CreateFigureUserEntryCommand } from "./createFigureUserEntryCommand";
+
+export const CreateFigureUserEntryCommandSchema = object().shape<
+  FormSchema<CreateFigureUserEntryCommand>
+>({
+  date: date().required(),
+  comment: string().optional(),
+  figureId: number().integer().required(),
+  figureUserStatusId: number().integer().required(),
+});
