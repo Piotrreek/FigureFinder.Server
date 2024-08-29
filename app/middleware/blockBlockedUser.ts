2509d1ec7cf513098ee784de7cd0f9ae5d9c4dce
Piotr Gębalski <piotrgebalski5@gmail.com>
2024-08-29 08:43:12 +0200

    Check whether user is blocked through call to database

--- app/middleware/blockBlockedUser.ts
+++ app/middleware/blockBlockedUser.ts
@@ -1,14 +1,26 @@
+import { PrismaClient } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { AuthenticatedRequest } from "./requireAuthenticated";
 
-const blockBlockedUserMiddleware = (
+const blockBlockedUserMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
     const authenticatedRequest = req as AuthenticatedRequest;
-    if (authenticatedRequest.user.blocked) {
+    const prisma = new PrismaClient();
+
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {
+        id: authenticatedRequest.user.userId,
+      },
+      select: {
+        blocked: true,
+      },
+    });
+
+    if (user.blocked) {
       res.status(403).send();
       return;
     }
